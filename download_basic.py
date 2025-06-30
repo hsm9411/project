77ea7908b34b7af62c4289a490bd480b5740b7dd
hsm@hsm <hsm@hsm>
2025-06-30 09:49:45 +0900

    250630 hsm_2

--- download_basic.py
+++ download_basic.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 import requests
 
-if not Path("notebook_utils.py").exists():
-    r = requests.get(
-        url="https://raw.githubusercontent.com/openvinotoolkit/openvino_notebooks/latest/utils/notebook_utils.py",
-    )
-    open("notebook_utils.py", "w").write(r.text)
+# if not Path("notebook_utils.py").exists():
+#     r = requests.get(
+#         url="https://raw.githubusercontent.com/openvinotoolkit/openvino_notebooks/latest/utils/notebook_utils.py",
+#     )
+#     open("notebook_utils.py", "w").write(r.text)
 
 
 if not Path("gradio_helper.py").exists():
